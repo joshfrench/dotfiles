b4c41f82cb3814e4165cea6ecacb13ba48042cb5
Josh French <josh@vitamin-j.com>
2014-01-21 10:20:52 -0800

    Additional battery indicator sweetness

--- battery-level.sh
+++ battery-level.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 percent=$(pmset -g batt | tail +2 | ack '(\d+)%' --output '$1')
-if [[ $percent -lt 16 ]]; then
-  echo -n "#[fg=brightred]"
-fi
-echo -n "⚡︎$percent"
+[[ $(pmset -g ac) =~ 'No adapter' ]] || charging='⚡︎'
+[[ $percent -gt 15 ]] || warning='#[fg=brightred]'
+echo -n "$warning$charging$percent"
