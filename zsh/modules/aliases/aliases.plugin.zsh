225ca656b222bf4066ccccb67fa7a0bbc9ff9238
Josh French <josh@vitamin-j.com>
2021-10-11 13:21:49 -0400

    rm medium aliases

--- zsh/modules/aliases/aliases.plugin.zsh
+++ zsh/modules/aliases/aliases.plugin.zsh
@@ -3,11 +3,9 @@ alias ll='exa -l'
 alias tmux='env TERM=screen-256color tmux'
 alias vim=nvim
 alias tf=terraform
-alias kubens='ok kubens'
 alias diff=colordiff
 alias cat=bat
 alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
-alias bs='bootstrap --sync'
 
 # git branch (interactive)
 function gb() {
