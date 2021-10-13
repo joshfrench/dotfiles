d7fa0671539881de536740c58334004bcdca5208
Josh French <josh.french@rstudio.com>
2021-10-13 18:17:38 -0400

    set up zsh env with privacy

--- .zsh_plugins.sh
+++ .zsh_plugins.sh
@@ -4,8 +4,8 @@ source /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-
 fpath+=( /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
 source /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 fpath+=( /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
-source /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-joshfrench-SLASH-pure/pure.plugin.zsh
-fpath+=( /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-joshfrench-SLASH-pure )
+source /Users/josh/src/joshfrench/pure/pure.plugin.zsh
+fpath+=( /Users/josh/src/joshfrench/pure )
 source /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
 fpath+=( /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
 source /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
@@ -14,11 +14,12 @@ source /Users/josh/dotfiles/zsh/modules/autojump/autojump.plugin.zsh
 fpath+=( /Users/josh/dotfiles/zsh/modules/autojump )
 source /Users/josh/dotfiles/zsh/modules/aliases/aliases.plugin.zsh
 fpath+=( /Users/josh/dotfiles/zsh/modules/aliases )
+source /Users/josh/dotfiles/zsh/modules/env/env.zsh
+source /Users/josh/dotfiles/zsh/modules/env/private.zsh
+fpath+=( /Users/josh/dotfiles/zsh/modules/env )
 source /Users/josh/dotfiles/zsh/modules/fzf/fzf.plugin.zsh
 fpath+=( /Users/josh/dotfiles/zsh/modules/fzf )
 source /Users/josh/dotfiles/zsh/modules/history/history.plugin.zsh
 fpath+=( /Users/josh/dotfiles/zsh/modules/history )
 source /Users/josh/dotfiles/zsh/modules/navigation/navigation.plugin.zsh
 fpath+=( /Users/josh/dotfiles/zsh/modules/navigation )
-source /Users/josh/dotfiles/zsh/modules/lazyload/lazyload.plugin.zsh
-fpath+=( /Users/josh/dotfiles/zsh/modules/lazyload )
---
+++ zsh/modules/env/env.zsh
@@ -0,0 +1,10 @@
+export EDITOR=nvim
+
+export NODE_PATH="/usr/local/lib/node"
+export GOPATH=$HOME/go
+export PATH="/usr/local/bin:/usr/local/sbin:$PATH:${GOROOT}/bin"
+
+export LSCOLORS="exgxBxdxcxegaxabagacad"
+export LS_COLORS='di=34;40:ln=36;40:so=1;;40:pi=33;40:ex=32;40:bd=34;46:cd=0;40:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
+
+# ./private.zsh is gitignored; store privileged items there
