bd38ebf5a9e3150e6e7abee5ffd9ff1fe0bdeebf
Josh French <josh.french@rstudio.com>
2021-10-15 17:02:02 -0400

    load pure from zsh/modules

--- .zsh_plugins.sh
+++ .zsh_plugins.sh
@@ -4,8 +4,6 @@ source /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-
 fpath+=( /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
 source /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 fpath+=( /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
-source /Users/josh/src/joshfrench/pure/pure.plugin.zsh
-fpath+=( /Users/josh/src/joshfrench/pure )
 source /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
 fpath+=( /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
 source /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
@@ -23,3 +21,5 @@ source /Users/josh/dotfiles/zsh/modules/history/history.plugin.zsh
 fpath+=( /Users/josh/dotfiles/zsh/modules/history )
 source /Users/josh/dotfiles/zsh/modules/navigation/navigation.plugin.zsh
 fpath+=( /Users/josh/dotfiles/zsh/modules/navigation )
+source /Users/josh/dotfiles/zsh/modules/pure/pure.plugin.zsh
+fpath+=( /Users/josh/dotfiles/zsh/modules/pure )
