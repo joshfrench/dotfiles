670bb786bd532b0eab7f2f9d8eba0a1e8e9cb1eb
Josh French <josh.french@rstudio.com>
2021-10-25 09:35:55 -0400

    better preview colorization

--- zsh/modules/env/env.zsh
+++ zsh/modules/env/env.zsh
@@ -7,4 +7,7 @@ export PATH="/usr/local/bin:/usr/local/sbin:$PATH:${GOROOT}/bin"
 export LSCOLORS="exgxBxdxcxegaxabagacad"
 export LS_COLORS='di=34;40:ln=36;40:so=1;;40:pi=33;40:ex=32;40:bd=34;46:cd=0;40:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
 
+export PISTOL_CHROMA_FORMATTER=terminal16m
+export PISTOL_CHROMA_STYLE=solarized-dark
+
 # ./private.zsh is gitignored; store privileged items there
