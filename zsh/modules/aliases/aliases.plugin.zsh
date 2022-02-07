b1f293019e7e4013926fac35e96b6dae71f38c9e
Josh French <josh.french@rstudio.com>
2022-02-07 13:17:58 -0500

    fzf aws profile selection

--- zsh/modules/aliases/aliases.plugin.zsh
+++ zsh/modules/aliases/aliases.plugin.zsh
@@ -10,9 +10,20 @@ alias koff='kubectl config unset current-context'
 
 # git branch (interactive)
 function gb() {
+  if [[ -n "$1" ]]; then git checkout "$1" && return 0; fi
   local branches selection branch
-  branches=$(git branch --sort=-committerdate -vv) &&
-    selection=$(echo "$branches" | fzf --height=20% +m) &&
+  # branches=$(git branch --sort=-committerdate -vv) &&
+  branches=$(git branch --column=plain --sort=committerdate --format='%(HEAD) %(align:width=36)%(refname:short)%(end) %(align:width=14)%(committerdate:relative)%(end) %(contents:subject) %(upstream:track)') &&
+     selection=$(echo "$branches" | fzf --tac --height=20% +m --cycle --layout=reverse) &&
     branch=$(echo "$selection" | awk '{print $1}' | sed "s/.* //") &&
     if [[ -t 1 ]]; then git checkout "$branch"; else echo $branch; fi
+}
+
+# AWS profile
+function ap() {
+  if [[ $1 == "-u" ]]; then unset AWS_PROFILE && return 0; fi
+  local profiles selection profile
+  profiles=$(aws configure list-profiles)
+  if [[ -n "$1" ]] && [[ "$profiles" =~ (^|[[:space:]])"$1"($|[[:space:]]) ]]; then selection="$1"; else selection=$(echo "$profiles" | fzf --cycle --height=10% --layout=reverse); fi
+  if [[ -t 1 ]]; then export AWS_PROFILE="$selection"; else echo "$selection"; fi
 }
