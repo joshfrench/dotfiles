5ac29350bdce8d246434704208814e83b4f818a4
Josh French <josh.french@rstudio.com>
2022-06-16 18:58:29 -0400

    lua all the things

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,10 @@
-vim.cmd [[
-  augroup packer_config
-    autocmd!
-    autocmd BufWritePost init.lua source <afile> | PackerSync
-  augroup end
-]]
+local packer_sync = vim.api.nvim_create_augroup('packer_sync', {clear = true})
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = 'init.lua',
+  group = packer_sync,
+  desc = 'Reload plugins on save',
+  command = 'source <afile> | PackerSync',
+})
 
 require 'user.settings'
 require 'user.plugins'
