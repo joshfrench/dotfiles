39b4ad31a155067d80b65681380290fe26c72c90
Josh French <josh.french@rstudio.com>
2022-06-16 11:29:10 -0400

    sure why not

--- .zsh_plugins.sh
+++ .zsh_plugins.sh
@@ -8,6 +8,8 @@ source /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-
 fpath+=( /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
 source /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
 fpath+=( /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-softmoth-SLASH-zsh-vim-mode/zsh-vim-mode.plugin.zsh
+fpath+=( /Users/josh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-softmoth-SLASH-zsh-vim-mode )
 source /Users/josh/dotfiles/zsh/modules/autojump/autojump.plugin.zsh
 fpath+=( /Users/josh/dotfiles/zsh/modules/autojump )
 source /Users/josh/dotfiles/zsh/modules/aliases/aliases.plugin.zsh
