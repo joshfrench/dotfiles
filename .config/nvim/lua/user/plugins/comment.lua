364684aa016862cb8b1fc21ee370f5bcc2c5cb74
Josh French <josh.french@rstudio.com>
2022-06-17 09:34:43 -0400

    Additional comment mappings

--- .config/nvim/lua/user/plugins/comment.lua
+++ .config/nvim/lua/user/plugins/comment.lua
@@ -39,30 +39,35 @@ end
 
 function M.insert_comment_above(...)
   local args = {...}
-  -- This includes the commentstring
   local configuration = comment.get_config(0)
   local line_number = args[1]
-  -- Get the current content of the line
-  local content = vim.api.nvim_buf_get_lines(0, line_number-2, line_number, false)[1]
-  --[[ Get the level of indentation of that line (Find the index of the first non-whitespace character) ]]
+  local content = vim.api.nvim_buf_get_lines(0, line_number-1, line_number, false)[1]
   local indentation = string.find(content, "%S")
-  --[[ Create a string with that indentation, with a dot at the end so that kommentary respects that indentation ]]
   local new_line = string.rep(" ", indentation-1) .. "."
-  -- Insert the new line underneath the current one
   vim.api.nvim_buf_set_lines(0, line_number-1, line_number-1, false, {new_line})
-  -- Comment in the new line
-  require('kommentary.kommentary').comment_in_line(line_number-1, configuration)
-  -- Set the cursor to the correct position
-  vim.api.nvim_win_set_cursor(0, {vim.api.nvim_win_get_cursor(0)[1]+1, #new_line-1})
-  -- Change the char under cursor (.)
+  require('kommentary.kommentary').comment_in_line(line_number, configuration)
+  vim.api.nvim_win_set_cursor(0, {vim.api.nvim_win_get_cursor(0)[1]-1, #new_line+2})
   vim.api.nvim_feedkeys("cl", "n", false)
 end
+
+function M.insert_comment_end(...)
+  local args = {...}
+  local commentstr = comment.get_config(0)[1]
+  vim.pretty_print(commentstr)
+  local line_number = args[1]
+  local content = vim.api.nvim_buf_get_lines(0, line_number-1, line_number, false)[1]
+  vim.api.nvim_win_set_cursor(0, {vim.api.nvim_win_get_cursor(0)[1], #content})
+  vim.api.nvim_feedkeys("a  "..commentstr.." ", "n", false)
+end
+
 --[[ This is a method provided by kommentary's config, it will take care of
 setting up a <Plug> mapping. The last argument is the optional callback
 function, meaning when we execute this mapping, this function will be
 called instead of the default. --]]
 comment.add_keymap("n", "kommentary_insert_below", comment.context.line, { expr = true }, M.insert_comment_below)
 comment.add_keymap("n", "kommentary_insert_above", comment.context.line, { expr = true }, M.insert_comment_above)
+comment.add_keymap("n", "kommentary_insert_end", comment.context.line, { expr = true }, M.insert_comment_end)
 -- Set up a regular keymapping to the new <Plug> mapping
 vim.api.nvim_set_keymap('n', '<C-_>o', '<Plug>kommentary_insert_below', { silent = true })
 vim.api.nvim_set_keymap('n', '<C-_>O', '<Plug>kommentary_insert_above', { silent = true })
+vim.api.nvim_set_keymap('n', '<C-_>A', '<Plug>kommentary_insert_end', { silent = true })
