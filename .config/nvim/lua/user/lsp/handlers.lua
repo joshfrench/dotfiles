e9b59e54a38f86902e7e8d086c74ad4aa26670a1
Josh French <josh.french@rstudio.com>
2022-06-17 16:34:20 -0400

    fixes

--- .config/nvim/lua/user/lsp/handlers.lua
+++ .config/nvim/lua/user/lsp/handlers.lua
@@ -60,11 +60,11 @@ M.on_attach = function(client, bufnr)
   lsp_keymap(bufnr)
 end
 
-M.capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local ok, cmp_lsp = pcall(require, 'cmp_nvim_lsp')
 if ok then
-  M.capabilities = cmp_lsp.update_capabilities(M.capabilities)
+  M.capabilities = cmp_lsp.update_capabilities(capabilities)
 end
 
 return M
--- .config/nvim/lua/user/nvim-tree.lua
+++ .config/nvim/lua/user/nvim-tree.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd("BufEnter", {
   nested = true,
   callback = function()
     if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
-      vim.cmd "quit"
+      -- vim.cmd "quit" -- TODO: fixme, check winnr
     end
   end
 })
