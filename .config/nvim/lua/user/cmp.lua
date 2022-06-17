518da7b8414d2ff4a638b4653bc2d3660beb4c74
Josh French <josh.french@rstudio.com>
2022-06-17 18:48:55 -0400

    config for lsp and friends

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -17,6 +17,39 @@ if not ok then
   return
 end
 
+local ok, cmp_buffer = pcall(require, 'cmp_buffer')
+if not ok then
+  return
+end
+
+local kind_icons = {
+  Text = "",
+  Method = "m",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
+
 cmp.setup({
   snippet = {
     expand = function(args) snippy.expand_snippet(args.body) end
@@ -28,10 +61,36 @@ cmp.setup({
     ['<TAB>'] = cmp.mapping(cmp_confirm, {'i'}),
   }),
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
+    { name = 'nvim_lsp_signature_help' },
+    { name = 'nvim_lsp', keyword_length = 3, max_item_count = 5 },
     { name = 'nvim_lua' },
     { name = 'buffer' },
     { name = 'path' },
     { name = 'emoji' }
-  })
+  }),
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    }
+  },
+  formatting = {
+    fields = {'kind', 'abbr', 'menu'},
+    format = function(entry, vim_item)
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[VIM]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      })[entry.source.name]
+      return vim_item
+    end,
+  }
 })
