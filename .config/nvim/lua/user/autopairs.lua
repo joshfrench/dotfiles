85efc1d39b02eff5848f3929e22ddf95773d8500
Josh French <josh.french@rstudio.com>
2022-06-18 18:07:24 -0400

    disable in telescope

--- .config/nvim/lua/user/autopairs.lua
+++ .config/nvim/lua/user/autopairs.lua
@@ -3,6 +3,14 @@ vim.g.AutoPairsMapBS = 1
 vim.g.AutoPairsMultilineBackspace = 1
 vim.g.AutoPairsFiletypeBlacklist = {'TelescopePrompt'}
 
+-- https://github.com/LunarWatcher/auto-pairs/issues/34
+local au = vim.api.nvim_create_augroup('autopairs', {clear = true})
+vim.api.nvim_create_autocmd('Filetype', {
+  pattern = 'TelescopePrompt',
+  group = au,
+  callback = function() vim.b.autopairs_enabled = false end
+})
+
 --[[ let g:AutoPairs = autopairs#AutoPairsDefine([
     {"open":"\w\zs<", "close":">", "filetype":"typescript"}
 ]) ]]
