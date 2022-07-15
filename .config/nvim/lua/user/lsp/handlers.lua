0ebf812c4822e6c090339fbc4811f82799f872bb
Josh French <josh.french@rstudio.com>
2022-07-15 14:13:53 -0400

    don't clobber 'r'

--- .config/nvim/lua/user/lsp/handlers.lua
+++ .config/nvim/lua/user/lsp/handlers.lua
@@ -59,7 +59,7 @@ local function lsp_keymap(bufnr)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
   vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
   vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-  vim.keymap.set('n', 'rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', 'Rn', vim.lsp.buf.rename, opts)
 end
 
 local function lsp_format(client, bufnr)
