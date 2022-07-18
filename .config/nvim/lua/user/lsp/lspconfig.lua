9f8c7588919b126e3f030e00aabeb29814ce36e4
Josh French <josh.french@rstudio.com>
2022-07-18 12:18:54 -0400

    split lspconfig into its own file

---
+++ .config/nvim/lua/user/lsp/lspconfig.lua
@@ -0,0 +1,18 @@
+local ok, lspconfig = pcall(require, 'lspconfig')
+if not ok then
+  return
+end
+
+for _, server in ipairs(require 'nvim-lsp-installer'.get_installed_servers()) do
+  local opts = {
+    on_attach = require 'user.lsp.handlers'.on_attach,
+    capabilities = require 'user.lsp.handlers'.capabilities,
+  }
+
+  local ok, server_opts = pcall(require, "user.lsp.settings." .. server.name)
+  if ok then
+    opts = vim.tbl_deep_extend("force", server_opts, opts)
+  end
+
+  lspconfig[server.name].setup(opts)
+end
