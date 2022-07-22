ff5fc59f686753b61aea3586c4f4e5147738272a
Josh French <josh.french@rstudio.com>
2022-07-22 09:25:40 -0400

    Add rename code action

--- .config/nvim/lua/user/null-ls.lua
+++ .config/nvim/lua/user/null-ls.lua
@@ -3,6 +3,30 @@ if not ok then
   return
 end
 
+local rename = {
+  method = null_ls.methods.CODE_ACTION,
+  filetypes = {},
+  generator = {
+    async = true,
+    fn = function(params, done)
+      local lspParams = {
+        textDocument = { uri = vim.uri_from_bufnr(params.bufnr) },
+        position = { line = params.row, character = params.col },
+      }
+      vim.lsp.buf_request(params.bufnr, "textDocument/prepareRename", lspParams, function(_, result)
+        if result ~= nil then
+          return done({{
+            title = 'Rename',
+            action = vim.lsp.buf.rename,
+          }})
+        else
+          return done()
+        end
+      end)
+    end
+  }
+}
+
 null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.shellcheck,
