619308d83d9bec0008bd133fd33097b5422de596
Josh French <josh.french@rstudio.com>
2022-08-10 09:38:43 -0400

    bat/fzf conf

--- zsh/modules/fzf/fzf.plugin.zsh
+++ zsh/modules/fzf/fzf.plugin.zsh
@@ -24,30 +24,23 @@ unset fzf_completion
 unset fzf_bindings
 
 _gen_fzf_default_opts() {
-  local color00='#002b36'
-  local color01='#073642'
-  local color02='#586e75'
-  local color03='#657b83'
-  local color04='#839496'
-  local color05='#93a1a1'
-  local color06='#eee8d5'
-  local color07='#fdf6e3'
-  local color08='#dc322f'
-  local color09='#cb4b16'
-  local color0A='#b58900'
-  local color0B='#859900'
-  local color0C='#2aa198'
-  local color0D='#268bd2'
-  local color0E='#6c71c4'
-  local color0F='#d33682'
+  local base03='#002b36'
+  local base02='#073642'
+  local base0='#839496'
+  local base2='#eee8d5'
+  local yellow='#b58900'
+  local green='#859900'
+  local cyan='#2aa198'
+  local blue='#268bd2'
 
   export FZF_DEFAULT_OPTS="
-    --color=bg+:$color01,bg:$color00,spinner:$color0B,hl:$color0D
-    --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C
-    --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D
-    --color=gutter:$color00
+    --color=bg+:$base02,bg:$base03,spinner:$green,hl:$blue
+    --color=fg:$base0,header:$blue,info:$yellow,pointer:$cyan
+    --color=marker:$cyan,fg+:$base2,prompt:$yellow,hl+:$blue
+    --color=gutter:$base03
     --cycle
     --layout=reverse
+    --preview='bat --style=numbers --color=always --line-range :500 {}'
   "
 }
 
