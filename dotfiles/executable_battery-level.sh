23cd1a43aa9a88625b29d5d01387c51ae7cc7b2c
Josh French <joshfrench@gmail.com>
2022-08-10 21:51:18 -0400

    ack -> rg

--- dotfiles/executable_battery-level.sh
+++ dotfiles/executable_battery-level.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-percent=$(pmset -g batt | tail +2 | ack '(\d+)%' --output '$1')
+percent=$(pmset -g batt | rg '\d+%' --only-matching)
 charging='#[bg=yellow]'
 [[ $(pmset -g ac) =~ 'No adapter' ]] || charging='#[bg=green]'
 [[ $percent -gt 15 ]] || warning='#[bg=red]'
-echo -n "$warning$charging $percent%"
+echo -n "$warning$charging $percent"
