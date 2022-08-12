841c54205133381496a74831c6ba2df13de46113
Josh French <joshfrench@gmail.com>
2022-08-12 16:40:56 -0400

    add rename action

--- private_dot_config/nvim/lua/user/null-ls.lua
+++ private_dot_config/nvim/lua/user/null-ls.lua
@@ -1,9 +1,12 @@
+local M = {}
+
 local ok, null_ls = pcall(require, 'null-ls')
 if not ok then
   return
 end
 
 local rename = {
+  name = "rename",
   method = null_ls.methods.CODE_ACTION,
   filetypes = {},
   generator = {
@@ -15,10 +18,10 @@ local rename = {
       }
       vim.lsp.buf_request(params.bufnr, "textDocument/prepareRename", lspParams, function(_, result)
         if result ~= nil then
-          return done({{
+          return done({ {
             title = 'Rename',
             action = vim.lsp.buf.rename,
-          }})
+          } })
         else
           return done()
         end
@@ -31,7 +34,19 @@ null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.shellcheck,
     -- null_ls.builtins.diagnostics.zsh,
-
     null_ls.builtins.code_actions.gitsigns,
   },
 })
+
+M.attach = function(client)
+  if client.resolved_capabilities.rename then
+    if not null_ls.is_registered('rename') then
+      null_ls.register(rename)
+    end
+    null_ls.enable('rename')
+  else
+    null_ls.disable('rename')
+  end
+end
+
+return M -- for use by user.lsp.handlers
