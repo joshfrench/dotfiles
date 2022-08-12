71c878d08d84ed72afb342c1777d2b7b0ea0d915
Josh French <joshfrench@gmail.com>
2022-08-12 16:22:21 -0400

    lil tweaks

--- dotfiles/zsh/modules/env/env.zsh
+++ dotfiles/zsh/modules/env/env.zsh
@@ -2,8 +2,7 @@ export EDITOR=nvim
 AUTO_PARAMS_SLASH=true
 export PYENV_SHELL=zsh
 
-export LSCOLORS="exgxBxdxcxegaxabagacad"
-export LS_COLORS='di=34;40:ln=36;40:so=1;;40:pi=33;40:ex=32;40:bd=34;46:cd=0;40:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
+export RIPGREP_CONFIG_PATH=~/.ripgreprc
 
 PURE_GIT_PULL=0
 PURE_PROMPT_SYMBOL='%%'
@@ -11,9 +10,6 @@ PURE_PROMPT_VICMD_SYMBOL=">"
 PURE_NEWLINE=1
 
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=11'
-
-export PISTOL_CHROMA_FORMATTER=terminal16m
-export PISTOL_CHROMA_STYLE=solarized-dark
 
 # ./private.zsh is gitignored; store privileged items there
 private=./private.zsh
