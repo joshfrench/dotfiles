1931c8ac9ebed1dad9740cf9650a0598aef4c57d
Josh French <joshfrench@gmail.com>
2022-08-12 16:45:09 -0400

    fix remaining hard-coded brew path

--- dotfiles/zsh/modules/lazyload/nvm.zsh
+++ dotfiles/zsh/modules/lazyload/nvm.zsh
@@ -2,7 +2,7 @@ nvm() {
   unfunction "$0"
 
   export NVM_DIR="$HOME/.nvm"
-  [ -s "${HOMEBREW_PREFIX}/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
+  [ -s "${HOMEBREW_PREFIX}/opt/nvm/nvm.sh" ] && . "${HOMEBREW_PREFIX}/opt/nvm/nvm.sh"
   [ -s "${HOMEBREW_PREFIX}/opt/nvm/etc/bash_completion.d/nvm" ] && . "${HOMEBREW_PREFIX}/opt/nvm/etc/bash_completion.d/nvm"
 
   $0 "$@"
