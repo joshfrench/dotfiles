511c1fb0ac161945df32ff7f3d22580a35f82049
Josh French <josh.french@rstudio.com>
2022-08-15 10:02:39 -0400

    pyenv update

--- dotfiles/zsh/modules/lazyload/pyenv.zsh
+++ dotfiles/zsh/modules/lazyload/pyenv.zsh
@@ -2,8 +2,13 @@ if [ $commands[pyenv] ]; then
   pyenv() {
     unfunction "$0"
 
-    source "${HOMEBREW_PREFIX}/Cellar/pyenv/2.2.5/completions/pyenv.zsh"
+    PATH="$(bash --norc -ec 'IFS=:; paths=($PATH); for i in ${!paths[@]}; do if [[ ${paths[i]} == "'/Users/josh/.pyenv/shims'" ]]; then unset '\''paths[i]'\''; fi; done; echo "${paths[*]}"')"
+    export PATH="/Users/josh/.pyenv/shims:${PATH}"
+    export PYENV_SHELL=zsh
+    source '/usr/local/Cellar/pyenv/2.3.3/libexec/../completions/pyenv.zsh'
+
     command pyenv rehash 2>/dev/null
+
     local command
     command="${1:-}"
     if [ "$#" -gt 0 ]; then
@@ -11,7 +16,7 @@ if [ $commands[pyenv] ]; then
     fi
 
     case "$command" in
-    rehash|shell)
+    activate|deactivate|rehash|shell)
       eval "$(pyenv "sh-$command" "$@")"
       ;;
     *)
