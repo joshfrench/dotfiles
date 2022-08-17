e44a989d84e6041beeace5587447068b94043cba
Josh French <josh.french@rstudio.com>
2022-08-17 17:20:53 -0400

    This is getting crazy
    `pyenv init -` installs its own function. but i want to lazy-load
    all the things, so now it's wrapped in a loading function that
    then overwrites itself with the upstream function.

--- dotfiles/zsh/modules/lazyload/pyenv.zsh
+++ dotfiles/zsh/modules/lazyload/pyenv.zsh
@@ -3,25 +3,31 @@ if [ $commands[pyenv] ]; then
     unfunction "$0"
 
     PATH="$(bash --norc -ec 'IFS=:; paths=($PATH); for i in ${!paths[@]}; do if [[ ${paths[i]} == "'/Users/josh/.pyenv/shims'" ]]; then unset '\''paths[i]'\''; fi; done; echo "${paths[*]}"')"
-    export PATH="/Users/josh/.pyenv/shims:${PATH}"
+    export PATH="${HOMEBREW_CELLAR}/pyenv-virtualenv/1.1.5/shims:/Users/josh/.pyenv/shims:${PATH}"
+    export PYENV_ROOT="$HOME/.pyenv"
     export PYENV_SHELL=zsh
+    export PYENV_VIRTUALENV_INIT=1
     source '/usr/local/Cellar/pyenv/2.3.3/libexec/../completions/pyenv.zsh'
 
     command pyenv rehash 2>/dev/null
 
-    local command
-    command="${1:-}"
-    if [ "$#" -gt 0 ]; then
-      shift
-    fi
+    eval 'pyenv() {
+      local command
+      command="${1:-}"
+      if [ "$#" -gt 0 ]; then
+        shift
+      fi
 
-    case "$command" in
-    activate|deactivate|rehash|shell)
-      eval "$(pyenv "sh-$command" "$@")"
-      ;;
-    *)
-      command pyenv "$command" "$@"
-      ;;
-    esac
+      case "$command" in
+      activate|deactivate|rehash|shell)
+        eval "$(pyenv "sh-$command" "$@")"
+        ;;
+      *)
+        command pyenv "$command" "$@"
+        ;;
+      esac
+    }'
+
+    pyenv "$@"
   }
 fi
