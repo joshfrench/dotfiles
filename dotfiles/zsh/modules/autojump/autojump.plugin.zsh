e880555569a8eef7b5e4026468a74d7ad92c8f6c
Josh French <josh.french@rstudio.com>
2022-08-18 10:55:24 -0400

    fix autojump colorization

--- dotfiles/zsh/modules/autojump/autojump.plugin.zsh
+++ dotfiles/zsh/modules/autojump/autojump.plugin.zsh
@@ -1,5 +1,29 @@
+__j() { # replace upstream j() with a version that doesn't do shitty colorization
+  if [[ ${1} == -* ]] && [[ ${1} != "--" ]]; then
+      autojump ${@}
+      return
+  fi
+
+  setopt localoptions noautonamedirs
+  local output="$(autojump ${@})"
+  if [[ -d "${output}" ]]; then
+      if [ -t 1 ]; then  # if stdout is a terminal, use colors
+              echo -e "\\033[0;34m${output}\\033[0m"
+      else
+              echo -e "${output}"
+      fi
+      cd "${output}"
+  else
+      echo "autojump: directory '${@}' not found"
+      echo "\n${output}\n"
+      echo "Try \`autojump --help\` for more information."
+      false
+  fi
+}
+
 j() {
-  unset -f j
  [ -f ${HOMEBREW_PREFIX}/etc/profile.d/autojump.sh ] && . ${HOMEBREW_PREFIX}/etc/profile.d/autojump.sh
- j "$@"
+  unset -f j
+ alias j=__j
+ __j "$@"
 }
