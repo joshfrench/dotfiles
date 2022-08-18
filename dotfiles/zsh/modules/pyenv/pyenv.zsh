bc534b546151f4ca8e6a8c521d0d3f96b18edfca
Josh French <josh.french@rstudio.com>
2022-08-18 10:35:46 -0400

    prompt to run pyenv

--- dotfiles/zsh/modules/pyenv/pyenv.zsh
+++ dotfiles/zsh/modules/pyenv/pyenv.zsh
@@ -7,7 +7,9 @@ _activate_pyenv() {
   elif [[ -f './Pipfile' ]]; then
     [[ -v PIPENV_ACTIVE ]] || echo -e "${GREEN}🐍 Run 'pipenv shell' to load Pipenv.${NC}"
   elif [[ -f './.python-version' ]]; then
-    [[ -v PYENV_VIRTUAL_ENV ]] || echo -e "${GREEN}🐍 Run 'pyenv activate' to load virtual env.${NC}"
+    [[ ! -v PYENV_VIRTUAL_ENV ]] && \
+      read -q "?$(echo -e ${GREEN}.python-version found, run \`pyenv activate\`?${NC}) (N/y) " && \
+      pyenv activate
   elif [ -f './requirements.txt' ] || [ -f './requirements.yml' ]; then
     echo -e "${GREEN}🐍 No venv found; create with 'python -m venv .venv'.${NC}"
   fi
