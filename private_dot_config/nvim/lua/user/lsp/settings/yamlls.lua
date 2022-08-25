b670d4c53fef635a9cacb7aa3d42b833c480e541
Josh French <josh.french@rstudio.com>
2022-08-25 10:15:21 -0400

    yamlls config

---
+++ private_dot_config/nvim/lua/user/lsp/settings/yamlls.lua
@@ -0,0 +1,67 @@
+local ok, yaml = pcall(require, 'yaml-companion')
+if not ok then
+  return
+end
+
+local cfg = yaml.setup({
+  builtin_matchers = {
+    kubernetes = { enabled = false },
+  },
+  schemas = {
+    result = {
+      {
+        name = "Kubernetes 1.26.3",
+        uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.23.6-standalone-strict/all.json"
+      },
+    },
+  },
+  lspconfig = {
+    filetypes = { "gotmpl", "yaml", "yaml.docker-compose" },
+    settings = {
+      yaml = {
+        customTags = {
+          "!Base64",
+          "!Cidr",
+          "!FindInMap sequence",
+          "!GetAtt",
+          "!GetAZs",
+          "!ImportValue",
+          "!Join sequence",
+          "!Ref",
+          "!Select sequence",
+          "!Split sequence",
+          "!Sub sequence",
+          "!Sub",
+          "!And sequence",
+          "!Condition",
+          "!Equals sequence",
+          "!If sequence",
+          "!Not sequence",
+          "!Or sequence",
+        },
+      }
+    }
+  }
+})
+
+return cfg
+
+--[[ return {
+  filetypes = { "gotmpl", "yaml", "yaml.docker-compose" },
+  settings = {
+    yaml = {
+      trace = {
+        server = "verbose"
+      },
+      schemas = {
+        ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.23.6-standalone/all.json"] = "/*",
+      },
+      schemaStore = {
+        url = "https://www.schemastore.org/api/json/catalog.json",
+        enable = true,
+      },
+      customTags = {
+      }
+    }
+  }
+} ]]
