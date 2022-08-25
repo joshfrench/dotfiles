2f12c570e4c3bb76bdeead55ed89b1ad88f1a064
Josh French <josh.french@rstudio.com>
2022-08-25 10:14:31 -0400

    add extra mapping

--- private_dot_config/nvim/lua/user/trouble.lua
+++ private_dot_config/nvim/lua/user/trouble.lua
@@ -14,43 +14,44 @@ trouble.setup({
   group = true, -- group results by file
   padding = true, -- add an extra new line on top of the list
   action_keys = { -- key mappings for actions in the trouble list
-      -- map to {} to remove a mapping, for example:
-      -- close = {},
-      close = "q", -- close the list
-      cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
-      refresh = "r", -- manually refresh
-      jump = {"<cr>", "<tab>"}, -- jump to the diagnostic or open / close folds
-      open_split = { "<c-x>" }, -- open buffer in new split
-      open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
-      open_tab = { "<c-t>" }, -- open buffer in new tab
-      jump_close = {"o"}, -- jump to the diagnostic and close the list
-      toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
-      toggle_preview = "P", -- toggle auto_preview
-      hover = "K", -- opens a small popup with the full multiline message
-      preview = "p", -- preview the diagnostic location
-      close_folds = {"zM", "zm"}, -- close all folds
-      open_folds = {"zR", "zr"}, -- open all folds
-      toggle_fold = {"zA", "za"}, -- toggle fold of current file
-      previous = "k", -- preview item
-      next = "j" -- next item
+    -- map to {} to remove a mapping, for example:
+    -- close = {},
+    close = "q", -- close the list
+    cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+    refresh = "r", -- manually refresh
+    jump = { "<cr>", "<tab>" }, -- jump to the diagnostic or open / close folds
+    open_split = { "<c-x>" }, -- open buffer in new split
+    open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
+    open_tab = { "<c-t>" }, -- open buffer in new tab
+    jump_close = { "o" }, -- jump to the diagnostic and close the list
+    toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+    toggle_preview = "P", -- toggle auto_preview
+    hover = "K", -- opens a small popup with the full multiline message
+    preview = "p", -- preview the diagnostic location
+    close_folds = { "zM", "zm" }, -- close all folds
+    open_folds = { "zR", "zr" }, -- open all folds
+    toggle_fold = { "zA", "za" }, -- toggle fold of current file
+    previous = "k", -- preview item
+    next = "j" -- next item
   },
   indent_lines = true, -- add an indent guide below the fold icons
   auto_open = false, -- automatically open the list when you have diagnostics
   auto_close = true, -- automatically close the list when you have no diagnostics
   auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
   auto_fold = false, -- automatically fold a file trouble list at creation
-  auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
+  auto_jump = { "lsp_definitions" }, -- for the given modes, automatically jump if there is only a single result
   signs = {
-      -- icons / text used for a diagnostic
-      error = "",
-      warning = "",
-      hint = "",
-      information = "",
-      other = "﫠"
+    -- icons / text used for a diagnostic
+    error = "",
+    warning = "",
+    hint = "",
+    information = "",
+    other = "﫠"
   },
   use_diagnostic_signs = true -- enabling this will use the signs defined in your lsp client
 })
 
 vim.keymap.set('n', 'tt', trouble.toggle)
-vim.keymap.set('n', 'tw', function() trouble.toggle({mode="workspace_diagnostics"}) end)
-vim.keymap.set('n', 'td', function() trouble.toggle({mode="document_diagnostics"}) end)
+vim.keymap.set('n', 'tw', function() trouble.toggle({ mode = "workspace_diagnostics" }) end)
+vim.keymap.set('n', 'td', function() trouble.toggle({ mode = "document_diagnostics" }) end)
+vim.keymap.set('n', 'tb', function() trouble.toggle({ mode = "document_diagnostics" }) end) -- b is for buffer, that's good enough for me
