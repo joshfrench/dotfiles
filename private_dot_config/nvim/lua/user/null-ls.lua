a86d8e41c3c1456883c926ab305adc8288df2d17
Josh French <josh.french@rstudio.com>
2022-08-25 10:13:07 -0400

    Add python linting/formatting

--- private_dot_config/nvim/lua/user/null-ls.lua
+++ private_dot_config/nvim/lua/user/null-ls.lua
@@ -42,6 +42,8 @@ null_ls.setup({
     null_ls.builtins.diagnostics.shellcheck,
     -- null_ls.builtins.diagnostics.zsh,
     null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.diagnostics.flake8,
   },
 })
 
