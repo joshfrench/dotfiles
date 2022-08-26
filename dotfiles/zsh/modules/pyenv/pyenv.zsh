41f459ba3814d2b6e72f3a3c6cc17a914f24c076
Josh French <josh.french@rstudio.com>
2022-08-26 10:08:17 -0400

    do less

--- dotfiles/zsh/modules/pyenv/pyenv.zsh
+++ dotfiles/zsh/modules/pyenv/pyenv.zsh
@@ -2,16 +2,22 @@ _activate_pyenv() {
   local GREEN="\033[0;32m"
   local NC="\033[0;0m"
 
-  if [[ -d './.venv' ]]; then
-    [[ -v VIRTUAL_ENV ]] || echo -e "${GREEN}🐍 Run '. .venv/bin/activate' to load venv.${NC}"
-  elif [[ -f './Pipfile' ]]; then
-    [[ -v PIPENV_ACTIVE ]] || echo -e "${GREEN}🐍 Run 'pipenv shell' to load Pipenv.${NC}"
-  elif [[ -f './.python-version' ]]; then
+  # if [[ -d './.venv' ]]; then
+  #   [[ -v VIRTUAL_ENV ]] || echo -e "${GREEN}🐍 Run '. .venv/bin/activate' to load venv.${NC}"
+  # elif [[ -f './Pipfile' ]]; then
+  #   [[ -v PIPENV_ACTIVE ]] || echo -e "${GREEN}🐍 Run 'pipenv shell' to load Pipenv.${NC}"
+  if [[ -f './.python-version' ]]; then
     [[ ! -v PYENV_VIRTUAL_ENV ]] && \
-      read -q "?$(echo -e ${GREEN}.python-version found, run \`pyenv activate\`?${NC}) (N/y) " && \
+      read -q "?$(echo -e ${GREEN}.python-version found, run \`pyenv activate\`?${NC}) (y/N) " && \
       pyenv activate
   elif [ -f './requirements.txt' ] || [ -f './requirements.yml' ]; then
-    echo -e "${GREEN}🐍 No venv found; create with 'python -m venv .venv'.${NC}"
+    read -q "yn?$(echo -e "${GREEN}No pyenv created, run \`make pyenv\`?${NC} (y/N)") "
+    if [ ${yn} = 'y' ]; then
+      echo "\n"
+      make pyenv
+      pyenv activate
+      echo "\n${GREEN}Next: aws sso and \`make deps\`.${NC}"
+    fi
   fi
 }
 
