0ba77e00a4c0b5e1495c920bd60b984a2fe2665c
Josh French <josh.french@rstudio.com>
2022-08-29 11:31:36 -0400

    add prettier (yamlls uses this under the hood anyway)

--- private_dot_config/nvim/lua/user/null-ls.lua
+++ private_dot_config/nvim/lua/user/null-ls.lua
@@ -10,6 +10,7 @@ null_ls.setup({
     null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.formatting.black,
     null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.formatting.prettierd,
   },
 })
 
