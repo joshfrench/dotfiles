7eaf7729b591d64fc2341ecc5d0e12f0b0aa799e
Josh French <josh.french@rstudio.com>
2022-09-08 09:53:45 -0400

    navic fixed this

--- private_dot_config/nvim/lua/user/lsp/handlers.lua
+++ private_dot_config/nvim/lua/user/lsp/handlers.lua
@@ -98,7 +98,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     local bufnr = args.buf
     local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client.server_capabilities.documentSymbolProvider and client.name ~= "bashls" then
+    if client.server_capabilities.documentSymbolProvider then
       require 'nvim-navic'.attach(client, bufnr)
     end
   end
