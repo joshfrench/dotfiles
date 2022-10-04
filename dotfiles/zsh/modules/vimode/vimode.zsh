ddfbc21a1a17a12a18d616300e76b5e6e077c2c9
Josh French <josh.french@rstudio.com>
2022-10-04 14:56:52 -0400

    Move vi widget to plugin
    most of these bindings can/should be moved to tmux conf for terminal
    agnosticity and pane awareness

---
+++ dotfiles/zsh/modules/vimode/vimode.zsh
@@ -0,0 +1,18 @@
+# jeffreytse/zsh-vi-mode
+ZVM_CURSOR_STYLE_ENABLED=true
+ZVM_VI_SURROUND_BINDKEY='s-prefix'
+ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
+ZVM_VI_HIGHLIGHT_BACKGROUND=#586e75
+ZVM_VI_HIGHLIGHT_FOREGROUND=#002b36
+function zvm_after_lazy_keybindings() {
+  bindkey -M viins '^R' fzf-history-widget
+  bindkey -M vicmd '^R' fzf-history-widget
+  bindkey -M vicmd '^[' zvm_enter_insert_mode
+}
+
+# softmoth/zsh-vi-mode
+# vim-mode-bindkey vicmd  -- change-surround sr
+# vim-mode-bindkey vicmd  -- delete-surround sd
+# vim-mode-bindkey vicmd  -- add-surround sa
+# vim-mode-bindkey vicmd  -- vi-insert '^['
+
