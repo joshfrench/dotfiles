4b6ca809538a5ef6403dbab09d61b914e69f5d83
Josh French <josh.french@rstudio.com>
2022-10-04 13:58:53 -0400

    exec just confuses chezmoi

--- run_once_install_homebrew.sh
+++ run_once_install_homebrew.sh
@@ -2,4 +2,4 @@
 
 brew_url=https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 echo "Installing homebrew from ${brew_url}"
-exec bash -c "$(curl -fsSL ${brew_url})"
+bash -c "$(curl -fsSL ${brew_url})"
