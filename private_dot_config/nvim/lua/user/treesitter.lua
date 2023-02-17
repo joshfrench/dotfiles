f69cfa789bd9caeb6b70309f836b1c132cfd05bb
Josh French <joshfrench@gmail.com>
2023-02-16 22:29:23 -0500

    more junk

--- private_dot_config/nvim/lua/user/treesitter.lua
+++ private_dot_config/nvim/lua/user/treesitter.lua
@@ -6,39 +6,40 @@ end
 local configs = require('nvim-treesitter.parsers').get_parser_configs()
 
 configs.gotmpl = {
-    install_info = {
-        url = "https://github.com/dannylongeuay/tree-sitter-go-template",
-        files = { "src/parser.c" }
-    },
-    filetype = "gotmpl",
-    used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl" }
+  install_info = {
+    url = "https://github.com/dannylongeuay/tree-sitter-go-template",
+    files = { "src/parser.c" }
+  },
+  filetype = "gotmpl",
+  used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl" }
 }
 
 treesitter.setup({
-    ensure_installed = {
-        'bash', 'dockerfile', 'go', 'gomod', 'gotmpl', 'javascript', 'json', 'lua',
-        'markdown', 'toml', 'tsx', 'typescript', 'python', 'rust', 'vim', 'yaml'
-    },
-    highlight = {
-        enabled = true,
-        additional_vim_regex_highlighting = true,
-    },
-    rainbow = {
-        enable = true,
-        extended_mode = true,
-        colors = { '#268bd2', '#2aa198', '#859900', '#b58900', '#cb4b16', '#6c71c4', },
-    },
-    context_commentstring = {
-        enable = true,
-    },
-    playground = {
-        enable = true,
-    },
-    query_linter = {
-        enable = true,
-        use_virtual_text = true,
-        lint_events = { "BufWrite", "CursorHold" },
-    },
+  ensure_installed = {
+    'bash', 'dockerfile', 'go', 'gomod', 'gotmpl', 'javascript', 'json', 'lua',
+    'make', 'markdown', 'ruby', 'terraform', 'toml', 'tsx', 'typescript', 'python',
+    'rust', 'vim', 'yaml'
+  },
+  highlight = {
+    enabled = true,
+    additional_vim_regex_highlighting = true,
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    colors = { '#268bd2', '#2aa198', '#859900', '#b58900', '#cb4b16', '#6c71c4', },
+  },
+  context_commentstring = {
+    enable = true,
+  },
+  playground = {
+    enable = true,
+  },
+  query_linter = {
+    enable = true,
+    use_virtual_text = true,
+    lint_events = { "BufWrite", "CursorHold" },
+  },
 })
 
 vim.opt.foldmethod = 'expr'
