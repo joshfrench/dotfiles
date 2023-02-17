a470954356b7887cd644fe8b10b7a0ccff5c3b29
Josh French <josh.french@posit.co>
2023-02-16 22:07:55 -0500

    match pycharm linter (no line length)

--- private_dot_config/nvim/lua/user/null-ls.lua
+++ private_dot_config/nvim/lua/user/null-ls.lua
@@ -6,14 +6,28 @@ end
 null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.shellcheck,
-    -- null_ls.builtins.diagnostics.zsh,
     null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.formatting.prettierd,
     -- null_ls.builtins.formatting.black,
     -- null_ls.builtins.diagnostics.flake8,
-    null_ls.builtins.formatting.autopep8, -- matches team pycharm
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.autopep8.with({ -- match team pycharm
+      generator_opts = {
+        command = 'autopep8',
+        to_stdin = true,
+        args = require('null-ls.helpers').range_formatting_args_factory({
+          '--max-line-length',
+          '200',
+          '-',
+        },
+          '--line-range',
+          nil,
+          { use_rows = true }),
+      },
+    }),
   },
 })
+
+
 
 --[[ This is broken :(
 
