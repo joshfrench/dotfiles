5224907d329ac1bed0aaf28e76f47cfeaa5dd26e
Josh French <josh.french@posit.co>
2023-03-06 09:13:20 -0500

    fix fzf build

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -16,7 +16,10 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope-fzf-native.nvim',
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+    },
     {
       dir = "~/src/joshfrench/yaml-companion.nvim/",
     }
