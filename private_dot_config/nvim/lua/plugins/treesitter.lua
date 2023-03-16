415d5e8d3698dee3d98db31ed7044100c06ce2a8
Josh French <joshfrench@gmail.com>
2023-03-16 08:05:23 -0400

    fix treesitter setup (finally)

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,40 +1,39 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   dependencies = {
-    -- 'p00f/nvim-ts-rainbow', -- unmaintained
-    -- 'mrjones2014/nvim-ts-rainbow', -- mainline
     'HiPhish/nvim-ts-rainbow2', -- active fork
     'JoosepAlviste/nvim-ts-context-commentstring',
     'nvim-treesitter/playground',
   },
   build = ':TSUpdate',
-  opts = {
-    ensure_installed = {
-      'bash', 'dockerfile', 'go', 'gomod', 'gotmpl', 'javascript', 'json', 'lua',
-      'make', 'markdown', 'markdown_inline', 'regex', 'ruby', 'terraform', 'toml',
-      'tsx', 'typescript', 'python', 'rust', 'vim', 'yaml'
-    },
-    highlight = {
-      enabled = true,
-      additional_vim_regex_highlighting = true,
-    },
-    rainbow = {
-      enable = true,
-      extended_mode = true,
-    },
-    context_commentstring = {
-      enable = true,
-    },
-    playground = {
-      enable = true,
-    },
-    query_linter = {
-      enable = true,
-      use_virtual_text = true,
-      lint_events = { "BufWrite", "CursorHold" },
-    },
-  },
-  init = function()
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = {
+        'bash', 'dockerfile', 'go', 'gomod', 'gotmpl', 'javascript', 'json', 'lua',
+        'make', 'markdown', 'markdown_inline', 'regex', 'ruby', 'terraform', 'toml',
+        'tsx', 'typescript', 'python', 'rust', 'vim', 'yaml'
+      },
+      highlight = {
+        enabled = true,
+        additional_vim_regex_highlighting = true,
+      },
+      rainbow = {
+        enable = true,
+        extended_mode = true,
+      },
+      context_commentstring = {
+        enable = true,
+      },
+      playground = {
+        enable = true,
+      },
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+    })
+
     local configs = require('nvim-treesitter.parsers').get_parser_configs()
 
     configs.gotmpl = {
@@ -45,7 +44,8 @@ return {
       filetype = "gotmpl",
       used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl" }
     }
-
+  end,
+  init = function()
     vim.opt.foldmethod = 'expr'
     vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
     vim.opt.indentexpr = 'treesitter#indentexpr()'
