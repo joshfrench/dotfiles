f1f1d6b5aa54563a498bd15a1ea6e1967815b86b
Josh French <josh.french@posit.co>
2023-03-17 16:01:46 -0400

    use colors module

--- private_dot_config/nvim/lua/user/scheme_overrides.lua
+++ private_dot_config/nvim/lua/user/scheme_overrides.lua
@@ -1,27 +1,11 @@
-local base03   = '#002b36'
-local base02   = '#073642'
-local base01   = '#586e75'
-local base00   = '#657b83'
-local base0    = '#839496'
-local base1    = '#93a1a1'
-local base2    = '#eee8d5'
-local base3    = '#fdf6e3'
-local yellow   = '#b58900'
-local orange   = '#cb4b16'
-local red      = '#dc322f'
-local magenta  = '#d33682'
-local violet   = '#6c71c4'
-local blue     = '#268bd2'
-local cyan     = '#2aa198'
-local green    = '#859900'
-local highlite = '#004b5e' -- additional midtone
+local colors = require('user.colors')
 
 for h, c in pairs({
-  include = green,
-  namespace = base1,
-  string = cyan,
-  ['punctuation.bracket'] = base1,
-  parameter = base1,
+  include = colors.green,
+  namespace = colors.base1,
+  string = colors.cyan,
+  ['punctuation.bracket'] = colors.base1,
+  parameter = colors.base1,
 }) do
   vim.api.nvim_set_hl(0, '@' .. h, { fg = c })
 end
