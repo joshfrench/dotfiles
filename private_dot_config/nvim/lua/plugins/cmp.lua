d4844979179ada93723c068ee10db347d159c6bc
Josh French <joshfrench@gmail.com>
2023-03-30 15:47:39 -0400

    cmp sorting leftovers

--- private_dot_config/nvim/lua/plugins/cmp.lua
+++ private_dot_config/nvim/lua/plugins/cmp.lua
@@ -65,13 +65,15 @@ return {
       }),
       sorting = {
         comparators = {
-          cmp.config.compare.offset,
-          cmp.config.compare.exact,
+          cmp.config.compare.locality,
+          cmp.config.compare.recently_used,
           cmp.config.compare.score,
-          cmp.config.compare.kind,
-          -- cmp.config.compare.sort_text,
-          cmp.config.compare.length,
+          cmp.config.compare.offset,
           cmp.config.compare.order,
+          -- cmp.config.compare.exact,
+          -- cmp.config.compare.kind,
+          -- cmp.config.compare.sort_text,
+          -- cmp.config.compare.length,
         }
       },
       formatting = {
