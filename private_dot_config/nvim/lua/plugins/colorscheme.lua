28b0648c42a68e1e39056bb6df5d1d3d1854faed
Josh French <joshfrench@gmail.com>
2023-04-15 19:33:37 -0400

    neo-tree colors

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -16,5 +16,16 @@ return {
     end
 
     vim.api.nvim_set_hl(0, '@constructor', {})
+
+    vim.api.nvim_set_hl(0, 'NeoTreeGitModified', { fg = colors.blue })
+    vim.api.nvim_set_hl(0, 'NeoTreeGitUntracked', { fg = colors.yellow })
+    vim.api.nvim_set_hl(0, 'NeoTreeGitUnstaged', { fg = colors.yellow })
+    vim.api.nvim_set_hl(0, 'NeoTreeGitStaged', { fg = colors.green })
+    vim.api.nvim_set_hl(0, 'NeoTreeRootName', { fg = colors.green })
+    vim.api.nvim_set_hl(0, 'NeoTreeModifiedSymbol', { fg = colors.red })
+    vim.api.nvim_set_hl(0, 'NeoTreeTabActive', { fg = colors.base2, bg = colors.base03 })
+    vim.api.nvim_set_hl(0, 'NeoTreeTabSeparatorActive', { bg = colors.base03, fg = colors.base01 })
+    vim.api.nvim_set_hl(0, 'NeoTreeTabInactive', { fg = colors.base00, bg = colors.base03 })
+    vim.api.nvim_set_hl(0, 'NeoTreeTabSeparatorInactive', { bg = colors.base03, fg = colors.base01 })
   end
 }
