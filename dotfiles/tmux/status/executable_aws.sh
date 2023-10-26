16ee324ed04cb08b83cbc5457e84eba01cca4d9c
Josh French <josh.french@posit.co>
2023-10-26 18:22:14 -0400

    too much aws

--- dotfiles/tmux/status/executable_aws.sh
+++ dotfiles/tmux/status/executable_aws.sh
@@ -8,13 +8,17 @@ _get_ap() {
 
 tmux_aws_profile() {
   [[ -f ${HOME}/.aws/sso/cache/^botocore*.json(#qN.mh+11) ]] && return # show nothing if creds are stale
-  local ap fg='#b58900'
+  local ap fg='#b58900' icon='󰅣'
   if [[ -n "$AWS_PROFILE" ]] && [[ -z $(_get_ap) ]]; then
     tmux set -q @aws-profile "$AWS_PROFILE"
   fi
   ap=$(_get_ap)
   if [[ -n "$ap" ]]; then
-    echo -n "#[fg=$fg]󰅣 ${ap}#[default]"
+    if [[ "$ap" == *production* ]]; then
+      fg='#cb4b16'
+      icon='󰅟'
+    fi
+    echo -n "#[fg=$fg]${icon} ${ap}#[default]"
   fi
 }
 
